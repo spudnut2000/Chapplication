1e59c0b317560557c038d11c0c85805c2eaf1a31
Chris Bell <chrisbell@hexstudios.co>
2024-01-30 00:17:02 -0600

    Added chat message when someone joins the server

--- app.gd
+++ app.gd
@@ -67,7 +67,7 @@ func add_peer(id, _username):
 	if PeerManager.connected_peers.has(id): return
 	#PeerManager.connected_peers[id] = _username
 	send_peer_information.rpc_id(1, id, _username)
-	# chat_box.text += PeerManager.connected_peers[id] + " has joined the server.\n"
+	chat_box.text += _username + " has joined the server.\n"
 
 
 @rpc("any_peer", "call_local")
@@ -79,7 +79,6 @@ func send_peer_information(id, usrname):
 	if multiplayer.is_server():
 		for i in PeerManager.connected_peers:
 			send_peer_information.rpc(i, PeerManager.connected_peers[i])
-			# chat_box.text += str(i, " ", PeerManager.connected_peers[i])
 
 
 func joined() -> void:
